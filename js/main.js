4447ed485323f4f7c92c3367dc0bb4b2b206e4a1
Devan Huapaya <huapayadevan@gmail.com>
2015-09-21 11:51:30 -0500

    changed name of menu file. changed around menu styles and animation times

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-console.log('called');
- 
+gbi = function ($id) {
+    return {
+        obj: document.getElementById($id),
+        addClass: function ($class) {
+            console.log($class);
+            this.obj.className = this.obj.className + " " + $class;
+        },
+        click: function ($funstuff) {
+            this.obj.onclick = $funstuff();
+        }
+    }
+}
+
+log = function ($msg) {
+    return console.log($msg);
+}
+
+var menuButton = gbi('menu--open-button');
+
+menuButton.obj.onclick = function () {
+    this.className = this.className + " open";
+}
+menuButton.click(function () {
+    menuButton.addClass('open');
+});
